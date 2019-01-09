d5b82e0624dd961cf42ce0d4c6c44e518d0d5695
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-01-10 00:24:45 +0530

    Added will_pagination gem

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -4,7 +4,7 @@ class PartiesController < ApplicationController
   # GET /parties
   # GET /parties.json
   def index
-    @parties = Party.all
+    @parties = Party.paginate(:page => params[:page], :per_page => 7)
   end
 
   # GET /parties/1
@@ -25,7 +25,7 @@ class PartiesController < ApplicationController
   # POST /parties.json
   def create
     @party = Party.new(party_params)
-    
+
     respond_to do |format|
       if @party.save
         format.html { redirect_to @party, notice: 'Party was successfully created.' }
